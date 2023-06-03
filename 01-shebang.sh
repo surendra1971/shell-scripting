863b3489cf560f7656eece5b5621b2bc2f8d9311
unknown <surendrapabolu@gmail.com>
2023-06-03 17:16:26 +0530

    updated shebang learning

---
+++ 01-shebang.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+## Start of the line in any bash script should always be a SHEBANG NOTATION.
+
+## Apart from the first line, none of the lines are considered as shebang
+## Shebang notatin should always start with #!
+
+## Using this SHEBANG Notation, we are telling the system the interpretor to use to run this script.
+
+## Your server would have java, go , python, nodejs, shell installed. ( interpretor )
+
+### Now if you run the script, how system will come to know which interpretor it has to use, that's where SHEBANG Notions helps
+
+#### How to run a linux bash script ? 
+
+    # *   bash scriptName.sh 
+    # *   sh scriptName.sh 
+    # *   ./scriptName
