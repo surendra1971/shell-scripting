808ade4c86c7566b089a8c5b127ccd9bb72ba24d
unknown <surendrapabolu@gmail.com>
2023-06-03 21:36:11 +0530

    updating the commands

---
+++ 07-commands.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+
+# There are 4 types of command available : 
+
+# 1) Binary                   ( /bin  , /sbin )
+# 2) Aliases                  ( Alises are shortcuts,  alias net="netstat -tulpn" )
+# 3) Shell Built-in Commands  
+# 4) Functions                # Functions are nothing but a set of command that can be written in a sequence and can be called n number of times as per your choice.
