5c5532243eeea5f2b068f1a8ea47b1f0cb119e99
unknown <surendrapabolu@gmail.com>
2023-06-04 21:46:37 +0530

    updating the commands

--- 07-commands.sh
+++ 07-commands.sh
@@ -6,3 +6,4 @@
 # 2) Aliases                  ( Alises are shortcuts,  alias net="netstat -tulpn" )
 # 3) Shell Built-in Commands  
 # 4) Functions                # Functions are nothing but a set of command that can be written in a sequence and can be called n number of times as per your choice.
+
