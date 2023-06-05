2c1ffcbafeb891dd811f6ebd1be3ac031b25e149
unknown <surendrapabolu@gmail.com>
2023-06-05 22:29:30 +0530

    updating the quotes

---
+++ 10-quotes.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+
+# In bash, output of your command will be complete changes based on the quotes you use : 
+# ""   : Double Quotes 
+# ''   : Single Quotes 
+
+a=53
+echo "Value of a is ${a}"
+echo Value of a is ${a}
+echo 'Value of a is ${a}'
+
+# '' : single quotes always eliminate the power of the special variable
