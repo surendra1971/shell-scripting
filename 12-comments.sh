6ee64fadc5d80783fba128029732e711490fa76a
unknown <surendrapabolu@gmail.com>
2023-06-05 22:31:42 +0530

    updating the comments

---
+++ 12-comments.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+
+
+# Comments enhance the verbosity of the scripts by making it more descriptive.
+
+# In a scirpt, if you want to test something and if you want to eliminate some lines and run the script, then you can comment then, If you command, system don't recorgnize it as executable.
+
+echo "This is to demonstrate the usage of single line and multi-line comments"
+
+<<COMMENT
+a=10 
+echo $a
+echo "In this infinite world we are just star dust"
+echo "This is to demonstrate multi line comments"
+COMMENT
