231380ca18d8b537d7796ae00b73aa6c4586a99d
unknown <surendrapabolu@gmail.com>
2023-06-06 11:12:13 +0530

    Auto push

--- robotshop/components/frontend.sh
+++ robotshop/components/frontend.sh
@@ -9,7 +9,7 @@ if [ $ID -ne 0 ] ; then
 fi
 
 echo "Installing nginx:"
-yum install nginx -y  &>>  "/tmp/frontend.log"
+yum install nginxfhjhd -y  &>>  "/tmp/frontend.log"
 if [ $? -eq 0 ] ; then
         echo "success"
 else 
