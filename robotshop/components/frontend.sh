a85ffc9559e9292f02fee67260aeec4729d3f53c
unknown <surendrapabolu@gmail.com>
2023-06-06 10:36:05 +0530

    Auto push

--- robotshop/components/frontend.sh
+++ robotshop/components/frontend.sh
@@ -1,2 +1,28 @@
 #!/bin/bash
-echo "I am an frontend"
+
+echo "Installing nginx: "
+yum install nginx -y
+
+
+
+
+
+# The frontend is the service in RobotShop to serve the web content over Nginx.
+
+# Install Nginx.
+
+# ```
+# # yum install nginx -y
+# # systemctl enable nginx
+# # systemctl start nginx
+
+# ```
+
+# Let's download the HTDOCS content and deploy it under the Nginx path.
+
+# ```
+# # curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+
+# ```
+
+# Deploy in Nginx Default Location.
