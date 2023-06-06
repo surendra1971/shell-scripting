680ed6fe88e778f547bf3721d68f118543729843
unknown <surendrapabolu@gmail.com>
2023-06-06 11:34:31 +0530

    Auto push

--- robotshop/components/frontend.sh
+++ robotshop/components/frontend.sh
@@ -1,38 +1,42 @@
 #!/bin/bash
 
-
+LOGFILE="/tmp/frontend.log"
 
 ID=$(id -u)
 if [ $ID -ne 0 ] ; then 
     echo -e "this script is run bt the root user"
     exit 1  
 fi
+stat() {
+    if [ $1 -eq 0 ] ; then
+            echo "success"
+    else 
+        echo "fail"
+        exit 2
+    fi
+}
 
 echo -n "Installing nginx:"
-yum install nginx -y  &>>  "/tmp/frontend.log"
-if [ $? -eq 0 ] ; then
-        echo "success"
-else 
-    echo "fail"
-fi
+yum install nginx -y  &>> LOGFILE
+stat $?
 
 echo -n "downloading the frontend component:"
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
-if [ $? -eq 0 ] ; then
-        echo "success"
-else 
-    echo "fail"
-fi
+stat $?
 
 echo -n "performing cleanup: "
 cd /usr/share/nginx/html
-rm -rf *    &>>  "/tmp/frontend.log"
+rm -rf *    &>>  LOGFILE
+stat $?
 
-if [ $? -eq 0 ] ; then
-        echo "success"
-else 
-    echo "fail"
-fi
+echo -n "Extracting frontend component: "
+
+unzip /tmp/frontend.zip    &>>  LOGFILE
+mv frontend-main/* .
+mv static/* .               &>>  LOGFILE
+rm -rf frontend-main README.md
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+stat $?
 
 # cd /usr/share/nginx/html
 # rm -rf *
