75832ae4de01e8d59d2c6457748af51f43414493
unknown <surendrapabolu@gmail.com>
2023-06-08 23:03:12 +0530

    updating the Auto Push

--- robotshop/components/frontend.sh
+++ robotshop/components/frontend.sh
@@ -1,23 +1,8 @@
 #!/bin/bash 
 
 COMPONENT=frontend
-LOGFILE="/tmp/${COMPONENT}.log"
 
-ID=$(id -u)
-
-if [ $ID -ne 0 ] ; then 
-    echo -e "\e[31m This script is expected to be run by a root user or with a sudo privilege \e[0m"
-    exit 1
-fi 
-
-stat() {
-    if [ $1 -eq 0 ] ; then 
-        echo -e "\e[32m success \e[0m"
-    else 
-        echo -e "\e[31m failure \e[0m"
-        exit 2
-    fi 
-}
+source components/common.sh
 
 echo -e "*********** \e[35m $COMPONENT Installation has started \e[0m ***********"
 
@@ -48,14 +33,10 @@ for component in catalogue user cart shipping payment; do
 done 
 stat $? 
 
-
 echo -n "Starting $COMPONENT service: "
+systemctl daemon-reload &>> $LOGFILE
 systemctl enable nginx  &>> $LOGFILE
-systemctl start nginx   &>> $LOGFILE
+systemctl restart nginx   &>> $LOGFILE
 stat $?
 
 echo -e "*********** \e[35m $COMPONENT Installation has Completed \e[0m ***********"
-
-
-
-# $ cd /home/roboshop
