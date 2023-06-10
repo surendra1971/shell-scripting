461484f890b36b70685a8daa48fad69dc8a13417
unknown <surendrapabolu@gmail.com>
2023-06-10 19:23:20 +0530

    updating the Auto Push

---
+++ robotshop/create-ec2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# AMI_ID="ami-0c1d144c8fdd8d690"  # hard codeing the AMI ID is not an best practice the should be need to come dynamically
+
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId'|sed -e 's/"//g')
+SG_ID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=b54-allow-all | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
+
+echo -e "AMI ID used to launch the EC2 is \e[35m $AMI_ID \e[0m"
+echo -e "Security Group ID used to launch the EC2 is \e[35m $SG_ID \e[0m"
+echo -e "\e[36m **** Launching Server **** \e[0m"
+
+IPADDRESS=$(aws ec2 run-instances  --image-id ${AMI_ID}  --instance-type t2.micro
